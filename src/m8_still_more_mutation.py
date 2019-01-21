a52889030ba84f070f4ca27f42c46205c9fbf32b
whitemg1 <whitemg1@rose-hulman.edu>
2019-01-21 12:04:21 -0500

    DONE

--- src/m8_still_more_mutation.py
+++ src/m8_still_more_mutation.py
@@ -5,7 +5,7 @@ In this module, you mutate by DELETING elements of a list.
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
          and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m6_mutation
 
@@ -65,13 +65,19 @@ def RETURN_delete_negatives(numbers):
       :type numbers: list
     where the list is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
     #             m6_mutation.run_test(...)
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+    new_list = []
+    for k in range(len(numbers)):
+        if numbers[k] >= 0:
+            new_list = new_list + [numbers[k]]
+
+    return new_list
 
 
 def run_test_MUTATE_delete_negatives():
@@ -124,7 +130,7 @@ def MUTATE_delete_negatives(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
@@ -138,6 +144,15 @@ def MUTATE_delete_negatives(numbers):
     #       then ask for help as needed.
     # HINT #2: Why might it be wise to start at the end and
     #       work backwards through the list to the beginning?
+
+
+    x = len(numbers)
+    for k in range(x - 1,-1,-1):
+        if numbers[k] < 0.0:
+            numbers.pop(k)
+
+
+
 
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
